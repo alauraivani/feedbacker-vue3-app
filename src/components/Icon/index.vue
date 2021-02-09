b4738fb28fa3d2114826ddf769606064ea657cf0
Anna Laura <alauraivani@gmail.com>
2021-02-09 15:34:29 -0300

    feat: add Icon component

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <component :is="name" v-bind="$props"/>
+
+</template>
+<script>
+
+import Loading from './Loading.vue'
+export default {
+  components: { Loading },
+  props: {
+    nome: { type: String, required: true }
+  }
+
+}
+</script>
